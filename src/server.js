a54b8c34681870e76c8d813191c72745fca44e46
Andrew Durrant <andrew.durrant75@gmail.com>
2020-03-31 16:15:32 -0600

    finish assignment

--- src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-const { PORT } = require('./config');
-
-app.listen(PORT, () => {
-  console.log(`Server listening in ${process.env.NODE_ENV} mode at http://localhost:${PORT}`);
-});
